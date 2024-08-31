4d98a8c6957f371ba906315155d693519680b139
Julius de Boer <45075461+JuliusDeBoer@users.noreply.github.com>
2024-08-31 17:09:13 +0200

    feat: Connect to wayland
    This is not gonna go well is it?

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,15 @@
 const std = @import("std");
+const wl = @cImport({
+    @cInclude("wayland-client.h");
+});
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // don't forget to flush!
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+    const display = wl.wl_display_connect(null);
+    if (display == null) {
+        std.debug.print("(wayland) Could not get display\n", .{});
+        return;
+    }
+    std.debug.print("(wayland) Connection established!\n", .{});
+    wl.wl_display_disconnect(display);
+    return;
 }
