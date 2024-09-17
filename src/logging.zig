05bbd5abe2366bc203373aa6913cc9fbcdc2833f
Julius de Boer <45075461+JuliusDeBoer@users.noreply.github.com>
2024-09-17 10:57:17 +0200

    refactor: Less error checking

--- src/logging.zig
+++ src/logging.zig
@@ -18,20 +18,20 @@ pub const Logger = struct {
     }
 
     pub fn info(self: Self, comptime fmt: []const u8, args: anytype) void {
-        stdout_writer.print("({s}) INFO: ", .{self.name}) catch return;
-        stdout_writer.print(fmt, args) catch return;
-        stdout_writer.print("\n", .{}) catch return;
+        _ = stdout_writer.print("({s}) INFO: ", .{self.name}) catch {};
+        _ = stdout_writer.print(fmt, args) catch {};
+        _ = stdout_writer.print("\n", .{}) catch {};
     }
 
     pub fn warn(self: Self, comptime fmt: []const u8, args: anytype) void {
-        stdout_writer.print("({s}) WARN: ", .{self.name}) catch return;
-        stdout_writer.print(fmt, args) catch return;
-        stdout_writer.print("\n", .{}) catch return;
+        _ = stdout_writer.print("({s}) WARN: ", .{self.name}) catch {};
+        _ = stdout_writer.print(fmt, args) catch {};
+        _ = stdout_writer.print("\n", .{}) catch {};
     }
 
     pub fn err(self: Self, comptime fmt: []const u8, args: anytype) void {
-        stderr_writer.print("({s}) ERR: ", .{self.name}) catch return;
-        stderr_writer.print(fmt, args) catch return;
-        stderr_writer.print("\n", .{}) catch return;
+        _ = stderr_writer.print("({s}) ERR: ", .{self.name}) catch {};
+        _ = stderr_writer.print(fmt, args) catch {};
+        _ = stderr_writer.print("\n", .{}) catch {};
     }
 };
